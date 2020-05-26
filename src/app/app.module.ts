4f5c1fe4948605bb31cac4f9cdc67f9547bae7e8
bjsamuel <chinnabhai944@gmail.com>
2020-05-26 09:52:06 +0530

    INITIAL COMMIT

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainResumeComponent } from './main-resume/main-resume.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainResumeComponent
   ],
   imports: [
     BrowserModule,
