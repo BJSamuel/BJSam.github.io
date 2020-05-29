51d61d6cc0f4e560713c61ab17ce5935bea824f5
BJS <josephsamuel8985@gmail.com>
2020-05-29 09:30:23 +0530

    added download options

--- src/app/rmenu/rmenu.component.ts
+++ src/app/rmenu/rmenu.component.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-
+import {faFileDownload, faInfo, faEnvelope} from '@fortawesome/free-solid-svg-icons'
 @Component({
   selector: 'app-rmenu',
   templateUrl: './rmenu.component.html',
   styleUrls: ['./rmenu.component.scss']
 })
 export class RMenuComponent implements OnInit {
-
+  DownloadIcon = faFileDownload;
+  infoIcon =faInfo;
+  mailIcon = faEnvelope;
   constructor() { }
 
   ngOnInit(): void {
